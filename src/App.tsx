f20366b1f0b2c99106d242a634b49fdc42981413
Hülya Ayar <99879948+hulyaayarr@users.noreply.github.com>
2024-02-05 19:56:41 +0300

    draggable functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,130 @@
-import "./App.css";
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function App() {
-  return <></>;
+type Quote = {
+  id: string;
+  content: string;
+};
+
+// fake data generator
+const initial = Array.from({ length: 10 }, (_, k) => k).map((k) => {
+  const custom: Quote = {
+    id: `id-${k}`,
+    content: `Quote ${k}`,
+  };
+  return custom;
+});
+
+const grid = 8;
+const reorder = (list, startIndex, endIndex): Quote[] => {
+  const result: Quote[] = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const QuoteItem = styled.div`
+  width: 200px;
+  border: 1px solid grey;
+  margin-bottom: ${grid}px;
+  background-color: lightblue;
+  padding: ${grid}px;
+`;
+
+function Quote({ quote, index }) {
+  return (
+    <Draggable key={quote.id} draggableId={quote.id} index={index}>
+      {(provided) => (
+        <QuoteItem
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {quote.content}
+        </QuoteItem>
+      )}
+    </Draggable>
+  );
 }
 
+const QuoteList = React.memo(function QuoteList({
+  quotes,
+}: {
+  quotes: Quote[];
+}) {
+  return quotes.map((quote: Quote, index: number) => (
+    <Quote quote={quote} index={index} key={quote.id} />
+  ));
+});
+
+/**
+ * Moves an item from one list to another list.
+ 
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  return result;
+};
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250,
+});
+*/
+function App() {
+  const [state, setState] = useState(initial);
+
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+    const quotes: Quote[] = reorder(
+      state,
+      result.source.index,
+      result.destination.index
+    );
+    setState(quotes);
+  }
+  return (
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <QuoteList quotes={state} />
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
 export default App;
