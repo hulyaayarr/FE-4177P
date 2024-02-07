e460b2f50ab542d632d2a4050048ebb62555c6b7
Hülya Ayar <99879948+hulyaayarr@users.noreply.github.com>
2024-02-08 00:44:58 +0300

    edit functionality trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row, Button, Modal, Form } from "react-bootstrap";
 import { TodoForm } from "./Form";
 
 type Todo = {
@@ -40,13 +40,55 @@ const getListStyle = (isDraggingOver: boolean) => ({
   padding: grid,
   width: 400,
 });
+interface ModalState {
+  show: boolean;
+}
 
-class App extends Component {
+class App extends Component<{
+  items: Todo[];
+  todos: Todo[];
+  inputValue: string;
+  modalState: ModalState;
+  editTodoId: string | null;
+}> {
   state = {
     items: getItems(0),
     todos: [] as Todo[],
     inputValue: "",
+    modalState: { show: false },
+    editTodoId: null,
   };
+
+  handleClose = () => {
+    this.setState({ modalState: { show: false } });
+  };
+
+  handleEditTodo = (id: string) => {
+    this.setState({ editTodoId: id, modalState: { show: true } });
+  };
+
+  handleShow = () => {
+    this.setState({ modalState: { show: true } });
+  };
+  handleSaveEdit = () => {
+    // Save the edited todo
+    // For simplicity, let's just update the content of the todo with the input value
+    const { todos, editTodoId, inputValue } = this.state;
+    const updatedTodos = todos.map((todo) =>
+      todo.id === editTodoId ? { ...todo, content: inputValue } : todo
+    );
+    this.setState({
+      todos: updatedTodos,
+      editTodoId: null,
+      modalState: { show: false },
+      inputValue: "",
+    });
+  };
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
   addTodo = (e) => {
     e.preventDefault();
 
@@ -60,10 +102,6 @@ class App extends Component {
         inputValue: "", // Clearing the input field after adding todo
       });
     }
-  };
-
-  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ inputValue: e.target.value });
   };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -83,6 +121,9 @@ class App extends Component {
 
       this.setState(state);
     }
+  };
+  handleNewInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ newInputValue: e.target.value });
   };
 
   render() {
@@ -146,6 +187,44 @@ class App extends Component {
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                               </svg>
                             </Button>
+                            <Button variant="primary" onClick={this.handleShow}>
+                              Edit
+                            </Button>
+                            <Modal
+                              show={this.state.modalState.show}
+                              onHide={this.handleClose}
+                            >
+                              <Modal.Header closeButton>
+                                <Modal.Title>Edit ToDo</Modal.Title>
+                              </Modal.Header>
+                              <Modal.Body>
+                                <Form>
+                                  <Form.Control
+                                    type="text"
+                                    placeholder="Enter todo"
+                                    onChange={this.handleNewInputChange}
+                                    value={inputValue}
+                                    style={{
+                                      maxWidth: "500px",
+                                    }}
+                                  />
+                                </Form>
+                              </Modal.Body>
+                              <Modal.Footer>
+                                <Button
+                                  variant="secondary"
+                                  onClick={this.handleClose}
+                                >
+                                  Close
+                                </Button>
+                                <Button
+                                  variant="primary"
+                                  onClick={this.handleSaveEdit}
+                                >
+                                  Save Changes
+                                </Button>
+                              </Modal.Footer>
+                            </Modal>
                           </div>
                         )}
                       </Draggable>
